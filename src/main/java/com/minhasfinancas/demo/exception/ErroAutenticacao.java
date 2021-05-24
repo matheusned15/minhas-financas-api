50a275438eb2d3b1eb8c70f0f2d1b27382013074
matheusned15 <matheusned15@gmail.com>
2021-05-24 01:02:56 -0300

    Implementando método de autenticar usuario

--- src/main/java/com/minhasfinancas/demo/exception/ErroAutenticacao.java
+++ src/main/java/com/minhasfinancas/demo/exception/ErroAutenticacao.java
@@ -1,6 +1,6 @@
 package com.minhasfinancas.demo.exception;
 
-public class ErroAutenticacao extends Exception {
+public class ErroAutenticacao extends RuntimeException {
 
 	public ErroAutenticacao(String mensagem) {
 		super(mensagem);
--- src/main/java/com/minhasfinancas/demo/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/minhasfinancas/demo/service/impl/UsuarioServiceImpl.java
@@ -26,13 +26,13 @@ public class UsuarioServiceImpl implements UsuarioService {
 	public Usuario autenticar(String email, String senha) {
 		Optional<Usuario> usuario = repository.findByEmail(email);
 
-//		if (!usuario.isPresent()) {
-//			throw new ErroAutenticacao("Usuário não encontrado para o email informado.");
-//		}
-//
-//		if (!usuario.get().getSenha().equals(senha)) {
-//			throw new ErroAutenticacao("Senha inválida.");
-//		}
+		if (!usuario.isPresent()) {
+			throw new ErroAutenticacao("Usuário não encontrado para o email informado.");
+		}
+
+		if (!usuario.get().getSenha().equals(senha)) {
+			throw new ErroAutenticacao("Senha inválida.");
+		}
 
 		return usuario.get();
 	}
