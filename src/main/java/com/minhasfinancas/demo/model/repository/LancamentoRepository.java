84e87ab34973368204b55bcd48ad53892a770c09
matheus.silveira <matheus.silveira@develcode.com.br>
2021-10-11 23:23:32 -0300

    Alterando método salvar usuário e criando Repository do Lancamento

--- src/main/java/com/minhasfinancas/demo/model/repository/LancamentoRepository.java
+++ src/main/java/com/minhasfinancas/demo/model/repository/LancamentoRepository.java
@@ -1,9 +1,23 @@
 package com.minhasfinancas.demo.model.repository;
 
+import com.minhasfinancas.demo.model.enums.StatusLancamento;
+import com.minhasfinancas.demo.model.enums.TipoLancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.minhasfinancas.demo.model.entity.Lancamento;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+    @Query( value =
+            " select sum(l.valor) from Lancamento l join l.usuario u "
+                    + " where u.id = :idUsuario and l.tipo =:tipo and l.status = :status group by u " )
+    BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
+            @Param("idUsuario") Long idUsuario,
+            @Param("tipo") TipoLancamento tipo,
+            @Param("status") StatusLancamento status);
 
 }
