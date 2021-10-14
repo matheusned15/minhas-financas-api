ec8042c6b5186673df9fddc65f4de52cce3c64c2
matheus.silveira <matheus.silveira@develcode.com.br>
2021-10-14 20:41:03 -0300

    Implementando o Post de usuários

---
+++ src/main/java/com/minhasfinancas/demo/resource/UsuarioResource.java
@@ -0,0 +1,40 @@
+package com.minhasfinancas.demo.resource;
+
+import com.minhasfinancas.demo.dto.UsuarioDTO;
+import com.minhasfinancas.demo.exception.RegraNegocioException;
+import com.minhasfinancas.demo.model.entity.Usuario;
+import com.minhasfinancas.demo.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+
+    private UsuarioService service;
+
+    public UsuarioResource(UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+
+        Usuario usuario = Usuario.builder()
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .senha(dto.getSenha())
+                .build();
+
+        try {
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
