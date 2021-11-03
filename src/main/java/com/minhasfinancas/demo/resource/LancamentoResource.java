f27bcccdf4e360d4b703e36559e5a46df2332c60
matheus.silveira <matheus.silveira@develcode.com.br>
2021-11-03 15:59:08 -0300

    Implementando o endpoint de deletar lançamentos

--- src/main/java/com/minhasfinancas/demo/resource/LancamentoResource.java
+++ src/main/java/com/minhasfinancas/demo/resource/LancamentoResource.java
@@ -35,33 +35,43 @@ public class LancamentoResource {
 
 
     @PostMapping
-    public ResponseEntity salvar(@RequestBody LancamentoDTO dto){
+    public ResponseEntity salvar(@RequestBody LancamentoDTO dto) {
         try {
             Lancamento entidade = converter(dto);
             entidade = service.salvar(entidade);
             return new ResponseEntity(entidade, HttpStatus.CREATED);
-        } catch (RegraNegocioException e){
+        } catch (RegraNegocioException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
 
     }
 
     @PutMapping("{id}")
-    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDTO dto){
-            return service.obterPorId(id).map( entity -> {
-                try {
-                    Lancamento lancamento = converter(dto);
-                    lancamento.setId(entity.getId());
-                    service.atualizar(lancamento);
-                    return ResponseEntity.ok(lancamento);
-                } catch (RegraNegocioException e){
-                    return ResponseEntity.badRequest().body(e.getMessage());
-                }
-            }).orElseGet( () ->
-                    new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDTO dto) {
+        return service.obterPorId(id).map(entity -> {
+            try {
+                Lancamento lancamento = converter(dto);
+                lancamento.setId(entity.getId());
+                service.atualizar(lancamento);
+                return ResponseEntity.ok(lancamento);
+            } catch (RegraNegocioException e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }).orElseGet(() ->
+                new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
     }
 
-    private Lancamento converter(LancamentoDTO dto){
+    @DeleteMapping("{id}")
+    public ResponseEntity deletar(@PathVariable("id") Long id) {
+        return service.obterPorId(id).map(entidade -> {
+            service.deletar(entidade);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }).orElseGet(() ->
+                new ResponseEntity("Lançamento não encontrado na base de Dados", HttpStatus.BAD_REQUEST));
+
+    }
+
+    private Lancamento converter(LancamentoDTO dto) {
         Lancamento lancamento = new Lancamento();
         lancamento.setId(dto.getId());
         lancamento.setDescricao(dto.getDescricao());
@@ -70,7 +80,7 @@ public class LancamentoResource {
         lancamento.setValor(dto.getValor());
 
         Usuario usuario = usuarioService.obterPorId(dto.getUsuario())
-                .orElseThrow( () -> new RegraNegocioException("Usuário não encontrando para o Id informado"));
+                .orElseThrow(() -> new RegraNegocioException("Usuário não encontrando para o Id informado"));
 
         lancamento.setUsuario(usuario);
         lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));
