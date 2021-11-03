ec9b2a87058e331d9476e7d3a162b97ab66f441d
matheus.silveira <matheus.silveira@develcode.com.br>
2021-11-03 15:53:06 -0300

    Implementando os endpoints de salvar e atualizar lançamentos

--- src/main/java/com/minhasfinancas/demo/resource/LancamentoResource.java
+++ src/main/java/com/minhasfinancas/demo/resource/LancamentoResource.java
@@ -36,8 +36,29 @@ public class LancamentoResource {
 
     @PostMapping
     public ResponseEntity salvar(@RequestBody LancamentoDTO dto){
-        return null;
+        try {
+            Lancamento entidade = converter(dto);
+            entidade = service.salvar(entidade);
+            return new ResponseEntity(entidade, HttpStatus.CREATED);
+        } catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDTO dto){
+            return service.obterPorId(id).map( entity -> {
+                try {
+                    Lancamento lancamento = converter(dto);
+                    lancamento.setId(entity.getId());
+                    service.atualizar(lancamento);
+                    return ResponseEntity.ok(lancamento);
+                } catch (RegraNegocioException e){
+                    return ResponseEntity.badRequest().body(e.getMessage());
+                }
+            }).orElseGet( () ->
+                    new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
     }
 
     private Lancamento converter(LancamentoDTO dto){
